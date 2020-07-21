6aa668b3275d5e23a3baa8343193c39c92a68679
Krystian Dużyński <krystian.duzynski@gmail.com>
2020-07-21 17:34:32 +0200

    refactored crc function

--- esolar_device/crc.py
+++ esolar_device/crc.py
@@ -2,14 +2,7 @@ from typing import Union, List
 
 
 def calculate_crc(data: Union[bytes, List[int]]) -> int:
-    chk = 0
-    for c in data:
-        chk += c
-    chk = chk & 0xff
-    if chk == 0:
-        return 0
-    chk = 255 - chk + 1
-    return chk
+    return -sum(data) & 0xff
 
 
 def verify_crc(header_data: bytes, payload_data: bytes, packet_crc: int) -> bool:
